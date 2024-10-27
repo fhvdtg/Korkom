206d490fe497e3bd8cd7981135e1cb6bbac2f366
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2024-10-27 21:00:35 +0100

    Add files via upload

---
+++ commands/core/help.js
@@ -0,0 +1,42 @@
+const { EmbedBuilder, GuildMemberFlagsBitField } = require("discord.js");
+const { Translate } = require("../../process_tools");
+
+module.exports = {
+    name: "help",
+    description: "All the commands this bot has!",
+    showHelp: false,
+
+    async execute({ client, inter }) {
+        const commands = client.commands.filter((x) => x.showHelp !== false);
+
+        const embed = new EmbedBuilder()
+            .setColor("#ff0000")
+            .setAuthor({
+                name: client.user.username,
+                iconURL: client.user.displayAvatarURL({
+                    size: 1024,
+                    dynamic: true,
+                }),
+            })
+            .setDescription(
+                await Translate(
+                   "Ahla Mariem Dabbegh",
+                ),
+            )
+            .addFields([
+                {
+                    name: `Enabled - ${commands.size}`,
+                    value: commands.map((x) => `\`${x.name}\``).join(" | "),
+                },
+            ])
+            .setTimestamp()
+            .setFooter({
+                text: await Translate(
+                    "Ahla Mariem Dabbegh",
+                ),
+                iconURL: inter.member.avatarURL({ dynamic: true }),
+            });
+
+        inter.editReply({ embeds: [embed] });
+    },
+};
